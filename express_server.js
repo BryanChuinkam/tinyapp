eef369df0b6f7d41c47c45ea6ac283a0757ffd20
bryanc.n <bryan.chuinkam@gmail.com>
2023-05-16 23:51:17 +0000

    Modified _header.ejs to display a username using cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,29 +24,19 @@ const generateRandomString = () => {
   return output;
 };
 
-app.get("/", (req, res) => {
-  res.send("hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { idShort: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { idShort: req.params.id, longURL: urlDatabase[req.params.id],  username: req.cookies["username"] };
   res.render("urls_show", templateVars);
   // res.redirect(urlDatabase[req.params.id]);
 });
@@ -57,7 +48,7 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/urls/:id/overhaul", (req, res) => {
-  if(req.body.edit === "edit"){
+  if (req.body.edit === "edit") {
     return res.redirect(`/urls/${req.params.id}`);
   }
   delete urlDatabase[req.params.id];
@@ -70,8 +61,22 @@ app.post("/urls/:id/update", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username) ;
+  res.cookie('username', req.body.username);
   res.redirect(`/urls`);
+});
+
+
+//NOT PART OF CORE FUNCTIONALITY
+app.get("/", (req, res) => {
+  res.send("hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
