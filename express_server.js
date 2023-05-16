4fb57fa594c66cd9d8562b513a8b603ff5a3f10f
bryanc.n <bryan.chuinkam@gmail.com>
2023-05-16 00:41:54 +0000

    added template for forms, added post route to receive form submission and log request body

--- express_server.js
+++ express_server.js
@@ -3,10 +3,24 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = () => {
+  let output = "";
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  
+  for (let count = 0; count <= 5; count++) {
+  	let letterIndex = Math.floor(Math.random() * 26);
+    output += letters[letterIndex];
+  }
+  
+  return output;
 };
 
 app.get("/", (req, res) => {
@@ -26,9 +40,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { idShort: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
